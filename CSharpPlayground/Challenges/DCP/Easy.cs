4438eeec2f75839d30d88f0a373d208a0b5486ef
Ryan Harris <ryan@servcraft.co.za>
2021-12-13 16:31:47 +0200

    Add project files.

---
+++ CSharpPlayground/Challenges/DCP/Easy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPlayground.Challenges.DCP
+{
+    public class Problem1
+    {
+        /*
+            TWO SUM
+            
+            This problem was recently asked by Google.
+            Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+            For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+            Bonus: Can you do this in one pass?
+        */
+
+        public bool Improved(int[] list, int k)
+        {
+            var seen = new HashSet<int>();
+            foreach (var i in list)
+            {
+                if (seen.Contains(k - i))
+                {
+                    return true;
+                }
+                seen.Add(i);
+            }
+            return false;
+        }
+
+        public void Solve()
+        {
+            var list = new int[] { 10, 15, 3, 7 };
+            var k = 17;
+            Console.WriteLine(Improved(list, k));
+        }
+    }
+}
---
+++ CSharpPlayground/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using CSharpPlayground.Challenges.DCP;
+
+namespace CSharpPlayground
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var p1 = new Problem1();
+            p1.Solve();
+        }
+    }
+}
