0f8e43b87cb9ff2e72c4ed8886c75fc9041ebbb0
Ryan Harris <ryan@servcraft.co.za>
2022-02-08 17:52:20 +0200

    Dependency inversion principle

--- CSharpPlayground/Program.cs
+++ CSharpPlayground/Program.cs
@@ -9,7 +9,7 @@ using CSharpPlayground.Algorithms.Search;
 using CSharpPlayground.Fundamentals;
 using CSharpPlayground.Algorithms.LinkedLists;
 using CSharpPlayground.Fundamentals.Generics;
-using CSharpPlayground.DesignPatterns.LiskovSubstitution;
+using CSharpPlayground.DesignPatterns.DependencyInversion;
 using CSharpPlayground.FSharpForFun;
 using HackerRank = CSharpPlayground.Challenges.HackerRank;
 using BinarySearch = CSharpPlayground.Challenges.BinarySearch;
@@ -23,7 +23,7 @@ namespace CSharpPlayground
             //new FSharpForFunDriver().Driver();
             //new HackerRank.Easy().Driver();
             //new BinarySearch.Easy().Driver();
-            new LiskovSubstitution().Driver();
+            new DependencyInversion().Driver();
         }
     }
 }
